0b9a312fa89e08cd75fd764d80c3e151b1bd3afe
Dy404 <diogodasilvaandradia@gmail.com>
2025-12-30 10:20:39 -0300

    Create Main.py
    Here's the main code, I made it clearer and more readable (my codes are kind of messy anyway ദ്ദി( •̀ ᴗ - ˵ )

---
+++ Main.py
@@ -0,0 +1,25 @@
+import random # random function
+import math # math function
+
+#========= Functions Section ==========
+
+# Generate a random key for an array.
+def newVal(val):
+    return math.floor(random.random() * val)
+
+#===================
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9] # All the digits
+characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] # All the characters of the alphabet
+special = ["@","#","$","%","*","&","§","=","+","?","/"] # All the special characters that can be used to confuse someone. 👍 
+
+n1 = len(numbers) # Numbers array length
+n2 = len(characters) # Characters array length
+n3 = len(special) # Special Characters array length
+
+kp = str(numbers[newVal(n1)]) + characters[newVal(n2)] + special[newVal(n3)] # Unique code in a single string.
+kp2 = str(numbers[newVal(n1)]) + characters[newVal(n2)] + special[newVal(n3)]
+kp3 = str(numbers[newVal(n1)]) + characters[newVal(n2)] + special[newVal(n3)]
+kp4 = str(numbers[newVal(n1)]) + characters[newVal(n2)] + special[newVal(n3)]
+
+print(kp + kp2 + kp3 + kp4) # I haven't automated this part yet ( ˶>˶˶<˶)
